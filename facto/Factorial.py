18c4c6c3a75f10627fb630a89a8a3b6ffec7326e
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-08-02 18:22:41 +0530

    Using if and elis statments for doing the code

--- facto/Factorial.py
+++ facto/Factorial.py
@@ -3,4 +3,8 @@ def recur_factorial(n):
        return n
    else:
        return n*recur_factorial(n-1)
-
+   num = 7
+   if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
